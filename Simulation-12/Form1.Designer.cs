19bad2b16633c72a2c6ee5f2521eff8cd4ebd32f
Elizaveta <42829488+sexypotato@users.noreply.github.com>
2021-06-10 23:36:18 +0700

    init

---
+++ Simulation-12/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace Simulation_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Team = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Wins = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Looses = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Draws = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Goals = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Score = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AvgGoals = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreateB = new System.Windows.Forms.Button();
+            this.NextB = new System.Windows.Forms.Button();
+            this.labWinner = new System.Windows.Forms.Label();
+            this.labDay = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Team,
+            this.Wins,
+            this.Looses,
+            this.Draws,
+            this.Goals,
+            this.Score,
+            this.AvgGoals});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 25;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(579, 271);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Team
+            // 
+            this.Team.HeaderText = "Team";
+            this.Team.MinimumWidth = 6;
+            this.Team.Name = "Team";
+            this.Team.ReadOnly = true;
+            this.Team.Width = 150;
+            // 
+            // Wins
+            // 
+            this.Wins.HeaderText = "Wins";
+            this.Wins.MinimumWidth = 6;
+            this.Wins.Name = "Wins";
+            this.Wins.ReadOnly = true;
+            this.Wins.Width = 50;
+            // 
+            // Looses
+            // 
+            this.Looses.HeaderText = "Looses";
+            this.Looses.MinimumWidth = 6;
+            this.Looses.Name = "Looses";
+            this.Looses.ReadOnly = true;
+            this.Looses.Width = 50;
+            // 
+            // Draws
+            // 
+            this.Draws.HeaderText = "Draws";
+            this.Draws.MinimumWidth = 6;
+            this.Draws.Name = "Draws";
+            this.Draws.ReadOnly = true;
+            this.Draws.Width = 50;
+            // 
+            // Goals
+            // 
+            this.Goals.HeaderText = "Goals";
+            this.Goals.MinimumWidth = 6;
+            this.Goals.Name = "Goals";
+            this.Goals.ReadOnly = true;
+            this.Goals.Width = 50;
+            // 
+            // Score
+            // 
+            this.Score.HeaderText = "Score";
+            this.Score.MinimumWidth = 6;
+            this.Score.Name = "Score";
+            this.Score.ReadOnly = true;
+            this.Score.Width = 50;
+            // 
+            // AvgGoals
+            // 
+            this.AvgGoals.HeaderText = "AvgGoals";
+            this.AvgGoals.MinimumWidth = 6;
+            this.AvgGoals.Name = "AvgGoals";
+            this.AvgGoals.ReadOnly = true;
+            this.AvgGoals.Visible = false;
+            this.AvgGoals.Width = 50;
+            // 
+            // CreateB
+            // 
+            this.CreateB.BackColor = System.Drawing.Color.LightGreen;
+            this.CreateB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateB.Location = new System.Drawing.Point(26, 304);
+            this.CreateB.Name = "CreateB";
+            this.CreateB.Size = new System.Drawing.Size(124, 38);
+            this.CreateB.TabIndex = 1;
+            this.CreateB.Text = "Сreate a Match";
+            this.CreateB.UseVisualStyleBackColor = false;
+            this.CreateB.Click += new System.EventHandler(this.CreateB_Click);
+            // 
+            // NextB
+            // 
+            this.NextB.BackColor = System.Drawing.Color.LightGreen;
+            this.NextB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NextB.Location = new System.Drawing.Point(181, 304);
+            this.NextB.Name = "NextB";
+            this.NextB.Size = new System.Drawing.Size(132, 58);
+            this.NextB.TabIndex = 2;
+            this.NextB.Text = "Next Day";
+            this.NextB.UseVisualStyleBackColor = false;
+            this.NextB.Click += new System.EventHandler(this.NextB_Click);
+            // 
+            // labWinner
+            // 
+            this.labWinner.AutoSize = true;
+            this.labWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labWinner.Location = new System.Drawing.Point(357, 322);
+            this.labWinner.Name = "labWinner";
+            this.labWinner.Size = new System.Drawing.Size(0, 20);
+            this.labWinner.TabIndex = 3;
+            // 
+            // labDay
+            // 
+            this.labDay.AutoSize = true;
+            this.labDay.Location = new System.Drawing.Point(507, 325);
+            this.labDay.Name = "labDay";
+            this.labDay.Size = new System.Drawing.Size(49, 17);
+            this.labDay.TabIndex = 3;
+            this.labDay.Text = "Day: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(599, 378);
+            this.Controls.Add(this.labDay);
+            this.Controls.Add(this.labWinner);
+            this.Controls.Add(this.NextB);
+            this.Controls.Add(this.CreateB);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Football Championship";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button CreateB;
+        private System.Windows.Forms.Button NextB;
+        private System.Windows.Forms.Label labWinner;
+        private System.Windows.Forms.Label labDay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Team;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Wins;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Looses;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Draws;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Goals;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Score;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AvgGoals;
+    }
+}
+
---
+++ Simulation-12/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_12
+{
+    public partial class Form1 : Form
+    {
+        private readonly Match match;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ReadOnly = false;
+            dataGridView1.RowCount = 8;
+            match = new Match();
+            Tools.InitGrid(dataGridView1);
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            NextB.Visible = false;
+        }
+
+        private void CreateB_Click(object sender, EventArgs e)
+        {
+            Tools.InitGrid(dataGridView1);
+            match.Day = 0;
+            bool flag = false;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (Convert.ToString(row.Cells["Team"].Value) == string.Empty)
+                    flag = true;
+            }
+
+            if (flag)
+                MessageBox.Show(@"Enter the team names!");
+            else
+            {
+                dataGridView1.ReadOnly = true;
+                for (int i = 0; i < dataGridView1.ColumnCount + 1; i++)
+                {
+                    dataGridView1["AvgGoals", i].Value = Tools.GenerateRandomAvg(); 
+                }
+
+                CreateB.Visible = false;
+                NextB.Visible = true;
+            }
+
+        }
+
+        private void NextB_Click(object sender, EventArgs e)
+        {
+            if (match.Day < 7)
+            {
+                match.ListMatches(dataGridView1);
+                labDay.Text = "Day: " + match.Day;
+            }
+            else
+            {
+                NextB.Visible = false;
+                CreateB.Visible = true;
+                dataGridView1.ReadOnly = false;
+                labWinner.Text = "Winner is " + Match.GetWinner(dataGridView1) + "!";
+            }
+        }
+
+        public static class Tools
+        {
+            private static Random random = new Random();
+
+            public static double GenerateRandomAvg()
+            {
+                return Math.Round(random.NextDouble() * 2, 1);
+            }
+
+            private static double GenerateDouble()
+            {
+                return random.NextDouble();
+            }
+
+            public static int GetTeamScore(double lambda)
+            {
+                int m = 0;
+                double S = 0;
+                while (S >= -lambda)
+                {
+                    double alpha = GenerateDouble();
+                    S += Math.Log(alpha);
+                    m++;
+                }
+
+                return m;
+            }
+
+            public static void InitGrid(DataGridView dataGrid)
+            {
+                Console.WriteLine(dataGrid.RowCount);
+                Console.WriteLine(dataGrid.ColumnCount);
+                for (int i = 0; i < dataGrid.RowCount; i++)
+                {
+                    for (int j = 1; j < dataGrid.ColumnCount - 1; j++)
+                    {
+                        dataGrid[j, i].Value = .0f;
+                    }
+                }
+            }
+        }
+
+        public class Match
+        {
+            public int Day = 0;
+            private readonly List<string> teams = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7" };
+            private readonly List<string> teams2 = new List<string>() { "1", "2", "3", "4", "5", "6", "7" }; //вместо teams.skip(1)
+
+            private static void PlayMatch(DataGridViewRow firstTeam, DataGridViewRow secondTeam)
+            {
+                int firstTeamScore = Tools.GetTeamScore((double)firstTeam.Cells["AvgGoals"].Value);
+                int secondTeamScore = Tools.GetTeamScore((double)secondTeam.Cells["AvgGoals"].Value);
+
+                firstTeam.Cells["Goals"].Value = Convert.ToInt32(firstTeam.Cells["Goals"].Value) + firstTeamScore;
+                secondTeam.Cells["Goals"].Value = Convert.ToInt32(secondTeam.Cells["Goals"].Value) + secondTeamScore;
+
+                if (firstTeamScore > secondTeamScore)
+                {
+                    firstTeam.Cells["Wins"].Value = Convert.ToInt32(firstTeam.Cells["Wins"].Value) + 1;
+                    firstTeam.Cells["Score"].Value = Convert.ToInt32(firstTeam.Cells["Score"].Value) + 3;
+                    secondTeam.Cells["Looses"].Value = Convert.ToInt32(secondTeam.Cells["Looses"].Value) + 1;
+                }
+                else if (firstTeamScore < secondTeamScore)
+
+                {
+                    secondTeam.Cells["Wins"].Value = Convert.ToInt32(secondTeam.Cells["Wins"].Value) + 1;
+                    secondTeam.Cells["Score"].Value = Convert.ToInt32(secondTeam.Cells["Score"].Value) + 3;
+                    firstTeam.Cells["Looses"].Value = Convert.ToInt32(firstTeam.Cells["Looses"].Value) + 1;
+                }
+                else
+                {
+                    firstTeam.Cells["Score"].Value = Convert.ToInt32(firstTeam.Cells["Score"].Value) + 1;
+                    secondTeam.Cells["Score"].Value = Convert.ToInt32(secondTeam.Cells["Score"].Value) + 1;
+                    firstTeam.Cells["Draws"].Value = Convert.ToInt32(firstTeam.Cells["Draws"].Value) + 1;
+                    secondTeam.Cells["Draws"].Value = Convert.ToInt32(secondTeam.Cells["Draws"].Value) + 1;
+                }
+            }
+
+            public static string GetWinner(DataGridView table)
+            {
+                int max = 0;
+                List<int> indexes = new List<int>();
+                for (int i = 0; i < table.RowCount; i++)
+                {
+                    if (max < Convert.ToInt32(table.Rows[i].Cells["Score"].Value))
+                    {
+                        max = Convert.ToInt32(table.Rows[i].Cells["Score"].Value);
+                    }
+                }
+
+                for (int i = 0; i < table.RowCount; i++)
+                {
+                    if (max == Convert.ToInt32(table.Rows[i].Cells["Score"].Value))
+                    {
+                        indexes.Add(i);
+                    }
+                }
+
+                if (indexes.Count == 1)
+                {
+                    return table.Rows[indexes[0]].Cells["Team"].Value.ToString();
+                }
+                else
+                {
+                    max = 0;
+                    int index = 0;
+                    foreach (var i in indexes.Where(i => max < Convert.ToInt32(table.Rows[i].Cells["Goals"].Value)))
+                    {
+                        max = Convert.ToInt32(table.Rows[i].Cells["Goals"].Value);
+                        index = i;
+                    }
+
+                    return table.Rows[index].Cells["Team"].Value.ToString();
+                }
+            }
+
+            public void ListMatches(DataGridView mat)
+            {
+                Console.WriteLine("Day {0}", (Day + 1));
+                PlayMatch(mat.Rows[Convert.ToInt32(teams2[Day])], (mat.Rows[Convert.ToInt32(teams[0])]));
+                Console.WriteLine("{0} vs {1}", teams2[Day], teams[0]);
+
+                for (int idx = 1; idx < 4; idx++)
+                {
+                    int firstTeam = Convert.ToInt32(teams2[(Day + idx) % 7]);
+                    int secondTeam = Convert.ToInt32(teams2[(Day + 7 - idx) % 7]);
+                    Console.WriteLine("{0} vs {1}", firstTeam, secondTeam);     
+                    PlayMatch(mat.Rows[firstTeam], (mat.Rows[secondTeam]));     
+                }
+
+                Day++;
+            }
+        }
+
+    }
+}
